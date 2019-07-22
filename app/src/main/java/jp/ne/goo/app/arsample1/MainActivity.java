a8877d94cabb7b4a3d0127b10bdfa9b77fd58756
ochi <mune.ochi@gmail.com>
2019-07-22 20:12:09 +0900

    3Dモデルが置ける

--- app/src/main/java/jp/ne/goo/app/arsample1/MainActivity.java
+++ app/src/main/java/jp/ne/goo/app/arsample1/MainActivity.java
@@ -1,23 +1,30 @@
 package jp.ne.goo.app.arsample1;
 
+import android.net.Uri;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.google.ar.core.Anchor;
 import com.google.ar.core.Frame;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.core.Trackable;
 import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import java.lang.ref.WeakReference;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private PointerDrawable pointer = new PointerDrawable();
     private boolean isTracking;
     private boolean isHitting;
+    private ModelLoader modelLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,20 +42,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         fragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
         fragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
             fragment.onUpdate(frameTime);
             onUpdate();
         });
+
+        modelLoader = new ModelLoader(new WeakReference<>(this));
+        initializeGallery();
     }
 
     private void onUpdate() {
@@ -102,6 +104,69 @@ public class MainActivity extends AppCompatActivity {
     private android.graphics.Point getScreenCenter() {
         View vw = findViewById(android.R.id.content);
         return new android.graphics.Point(vw.getWidth()/2, vw.getHeight()/2);
+    }
+
+    private void initializeGallery() {
+        LinearLayout gallery = findViewById(R.id.gallery_layout);
+
+        ImageView andy = new ImageView(this);
+        andy.setImageResource(R.drawable.droid_thumb);
+        andy.setContentDescription("andy");
+        andy.setOnClickListener(view ->{addObject(Uri.parse("andy_dance.sfb"));});
+        gallery.addView(andy);
+
+        ImageView cabin = new ImageView(this);
+        cabin.setImageResource(R.drawable.cabin_thumb);
+        cabin.setContentDescription("cabin");
+        cabin.setOnClickListener(view ->{addObject(Uri.parse("Cabin.sfb"));});
+        gallery.addView(cabin);
+
+        ImageView house = new ImageView(this);
+        house.setImageResource(R.drawable.house_thumb);
+        house.setContentDescription("house");
+        house.setOnClickListener(view ->{addObject(Uri.parse("House.sfb"));});
+        gallery.addView(house);
+
+        ImageView igloo = new ImageView(this);
+        igloo.setImageResource(R.drawable.igloo_thumb);
+        igloo.setContentDescription("igloo");
+        igloo.setOnClickListener(view ->{addObject(Uri.parse("igloo.sfb"));});
+        gallery.addView(igloo);
+    }
+
+    private void addObject(Uri model) {
+        Frame frame = fragment.getArSceneView().getArFrame();
+        android.graphics.Point pt = getScreenCenter();
+        List<HitResult> hits;
+        if (frame != null) {
+            hits = frame.hitTest(pt.x, pt.y);
+            for (HitResult hit : hits) {
+                Trackable trackable = hit.getTrackable();
+                if (trackable instanceof Plane &&
+                        ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                    modelLoader.loadModel(hit.createAnchor(), model);
+                    break;
+
+                }
+            }
+        }
+    }
+
+    public void addNodeToScene(Anchor anchor, Renderable renderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode node = new TransformableNode(fragment.getTransformationSystem());
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+        fragment.getArSceneView().getScene().addChild(anchorNode);
+        node.select();
+    }
+
+    public void onException(Throwable throwable){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(throwable.getMessage())
+                .setTitle("Codelab error!");
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     @Override
